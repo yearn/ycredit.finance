59440ee818115f2b6761101597029f5ea135aaac
Andre Cronje <andre.cronje@yearn.finance>
2020-10-15 13:08:36 +0200

    Basic 0 coverage tests
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- test/StableCreditProtocol.js
+++ test/StableCreditProtocol.js
@@ -8,7 +8,18 @@ contract("StableCreditProtocol", accounts => {
       assert.equal(
         link.valueOf(),
         0x271bf4568fb737cc2e6277e9B1EE0034098cDA2a,
-        "invalid link"
+        "invalid LINK"
+      );
+    })
+  );
+  it("UNI must be set to 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", () =>
+    StableCreditProtocol.deployed()
+    .then(instance => instance.UNI.call())
+    .then(uni => {
+      assert.equal(
+        uni.valueOf(),
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D,
+        "invalid UNI"
       );
     })
   );
